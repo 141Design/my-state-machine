e267354b8c46c034600a9c78c1f7b638357db4e7
141Design <141.atlas@gmail.com>
2021-05-05 22:16:29 +0900

    add : ステートマシンのようなものを試作

--- Program.cs
+++ Program.cs
@@ -4,12 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//ステートマシンってこんな感じ？
+
 namespace SimpleStateMachine
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StateMachine machine = new StateMachine();
+            State s1 = new State();
+            State s2 = new State();
+            State s3 = new State();
+            State st = new State();
+            States states = new States();
+
+            s1.Process = states.state1;
+            s2.Process = states.state2;
+            s3.Process = states.state3;
+            machine.AddState(s1, "s1");
+            machine.AddState(s2, "s2");
+            machine.AddState(s3, "s3");
+            machine.AddState(st, "end");
+            machine.Jump("s1");
+            while (machine.PresentState != "end")
+            {
+                machine.Execute();
+            }   
         }
     }
+
+    //ダミー用のクラス
+    public class States
+    {
+
+        int count;
+
+        public States()
+        {
+            count = 0;
+        }
+
+        public string state1(string presentState, string eventStr)
+        {
+            Console.WriteLine("state1 Called");
+            if(count >= 5)
+            {
+                count = 0;
+                return "s2";
+            }
+            count++;
+            return presentState;
+        }
+
+        public string state2(string presentState, string eventStr)
+        {
+
+            Console.WriteLine("state2 Called");
+            if (count >= 5)
+            {
+                count = 0;
+                return "s3";
+            }
+            count++;
+            return presentState;
+        }
+
+        public string state3(string presentState, string eventStr)
+        {
+            Console.WriteLine("state3 Called");
+            if (count >= 5)
+            {
+                count = 0;
+                return "end";
+            }
+            count++;
+            return presentState;
+        }
+        public string terminalState(string presentState, string eventStr)
+        {
+            Console.WriteLine("end");
+            return presentState;
+        }
+    }
+
 }
---
+++ StateMachine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//状態の初期化メソッド、実行メソッド、アクションと遷移先テーブルを登録する
+//動詞stringを入力すると別の状態に遷移する
+
+namespace SimpleStateMachine
+{
+    public class StateMachine
+    {
+        public string PresentState{ set; get; }
+        public Dictionary<string, State> StateTable;
+
+        public StateMachine()
+        {
+            StateTable = new Dictionary<string, State>();
+        }
+
+        //状態を追加する 状態を追加できない場合は-1を返す。
+        public int AddState(State newState,string stateName)
+        {
+            //キーが存在しない場合に登録する
+            if (!StateTable.ContainsKey(stateName))
+            {
+                StateTable[stateName] = newState;
+                return 0;
+            }
+            return -1;
+        }
+
+        //状態を実行する。
+        public int Execute()
+        {
+            if(StateTable[PresentState]!= null)
+            {
+                if(StateTable[PresentState].Process != null)
+                {
+                    PresentState = StateTable[PresentState].Process(PresentState,null);
+                    return 0;
+                }
+                return -1;//メソッドが登録されていない
+            }
+            return -2;//状態が登録されていない
+        }
+
+        //指定の状態に遷移する
+        public int Jump(string stateName)
+        {
+            if (StateTable.ContainsKey(stateName))
+            {
+                PresentState = stateName;
+            }
+            return 0;
+        }
+        
+        public List<string> GetStateNames()
+        {
+            List<string> retStateNames = new List<string>(StateTable.Keys);
+            return retStateNames;
+        }
+    }
+
+    public class State
+    {
+        public Func<string,string, string> Process { get; set; }// Process 現在の状態,イベント文字列, 戻り値：次の状態
+    }
+}
